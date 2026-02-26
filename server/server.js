1fedf680cb424157cea6dfa0d0ee6da223530e32
Demo <naiks0234@gmail.com>
2026-02-26 20:50:26 +0530

    Fix render port binding

--- server/server.js
+++ server/server.js
@@ -1,11 +1,11 @@
-require("dotenv").config();
-const connectDB = require("./src/config/db");
 const app = require("./src/app");
+const connectDB = require("./src/config/db");
+require("dotenv").config();
 
 connectDB();
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
