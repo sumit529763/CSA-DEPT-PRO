143b82dedf997ea27e64086133585da6b1e6a570
Demo <naiks0234@gmail.com>
2026-03-22 14:39:33 +0530

    backend 2nd deploy

--- server/src/app.js
+++ server/src/app.js
@@ -4,56 +4,14 @@ const cors = require("cors");
 
 const app = express();
 
-<<<<<<<<< Temporary merge branch 1
-// ✅ Allowed Origins
-=========
-// Allowed Origins
->>>>>>>>> Temporary merge branch 2
-const allowedOrigins = [
-  "http://localhost:5173",
-  "https://csa-dept-pro.web.app",
-  "https://csa-dept-pro.firebaseapp.com"
-];
-
-<<<<<<<<< Temporary merge branch 1
-=========
-// CORS Middleware
->>>>>>>>> Temporary merge branch 2
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      if (!origin) return callback(null, true);
-
-<<<<<<<<< Temporary merge branch 1
-      if (allowedOrigins.indexOf(origin) !== -1) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-=========
-      if (allowedOrigins.includes(origin)) {
-        return callback(null, true);
-      } else {
-        return callback(new Error("Not allowed by CORS"));
->>>>>>>>> Temporary merge branch 2
-      }
-    },
-    credentials: true,
-  })
-);
-
+app.use(cors());
 app.use(express.json());
 
-<<<<<<<<< Temporary merge branch 1
-// Serve uploaded images (if needed)
+// // 🔥 Serve uploaded images
 app.use("/uploads", express.static("uploads"));
 
 // Routes
-=========
-// Serve uploaded images
-app.use("/uploads", express.static("uploads"));
 
-/* ROUTES */
->>>>>>>>> Temporary merge branch 2
 app.use("/api/auth", require("./routes/auth.routes"));
 app.use("/api/test", require("./routes/test.routes"));
 app.use("/api/news", require("./routes/news.routes"));
